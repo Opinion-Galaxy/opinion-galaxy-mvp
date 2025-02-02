0c3b03c1404a15b2cc36196b8fd8fbc56dd72e8c
edegp <a.yuhi1164@gmail.com>
2025-02-02 14:18:30 +0900

    fix ip and modify jq command

--- run.sh
+++ run.sh
@@ -16,16 +16,9 @@ PROJECT_ID=$(curl -s -H "Metadata-Flavor: Google" \
 NOW=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 ONE_MINUTE_AGO=$(date -u -d '-1 minute' +"%Y-%m-%dT%H:%M:%SZ")
 
-# ネットワーク情報の表示
-echo "IP アドレス一覧:"
-ip addr show
-
-export HOSTNAME=$(hostname -I)
-echo "ホストのIP（hostname -I）: $HOSTNAME"
-
 export PODS=$(curl -s -H "Authorization: Bearer ${TOKEN}" \
   "https://monitoring.googleapis.com/v3/projects/${PROJECT_ID}/timeSeries?filter=metric.type%3D%22run.googleapis.com/container/instance_count%22&interval.startTime=${ONE_MINUTE_AGO}&interval.endTime=${NOW}" \
-  | jq -r '.timeSeries[] | map(select(.metric.labels.state == "active")) | .points[0].value.int64Value')
+  | jq -r '.timeSeries[] | map(select(.metric.labels.state? == "active")) | .points[0]?.value.int64Value // "null"')
 
 echo "Pods: $PODS"
 
