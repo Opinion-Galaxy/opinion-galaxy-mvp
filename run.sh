236c8486d29b4f27f44c2e98c5e04f760336d12f
edegp <a.yuhi1164@gmail.com>
2025-02-02 12:56:33 +0900

    fix

--- run.sh
+++ run.sh
@@ -10,19 +10,15 @@ trap 'error_handler $LINENO' ERR
 TOKEN=$(curl -s -H "Metadata-Flavor: Google" \
   "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token" | jq -r '.access_token')
 
-echo $TOKEN
-
 PROJECT_ID=$(curl -s -H "Metadata-Flavor: Google" \
   "http://metadata.google.internal/computeMetadata/v1/project/project-id")
-
-echo $PROJECT_ID
 
 NOW=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 ONE_MINUTE_AGO=$(date -u -d '-1 minute' +"%Y-%m-%dT%H:%M:%SZ")
 
 export PODS=$(curl -s -H "Authorization: Bearer ${TOKEN}" \
   "https://monitoring.googleapis.com/v3/projects/${PROJECT_ID}/timeSeries?filter=metric.type%3D%22run.googleapis.com/container/instance_count%22&interval.startTime=${ONE_MINUTE_AGO}&interval.endTime=${NOW}" \
-  | jq -r '.timeSeries[] | select(.labels.state == "active")' | jq -r '.points[0].value.int64Value')
+  | jq -r '.timeSeries[] | map(select(.labels.state == "active")) | .points[0].value.int64Value')
 
 echo "Pods: $PODS"
 
