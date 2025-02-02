55dd02862280bf11ac1258fe866be5b150f064cf
edegp <a.yuhi1164@gmail.com>
2025-02-02 12:46:27 +0900

    fix

--- run.sh
+++ run.sh
@@ -22,7 +22,7 @@ ONE_MINUTE_AGO=$(date -u -d '-1 minute' +"%Y-%m-%dT%H:%M:%SZ")
 
 export PODS=$(curl -s -H "Authorization: Bearer ${TOKEN}" \
   "https://monitoring.googleapis.com/v3/projects/${PROJECT_ID}/timeSeries?filter=metric.type%3D%22run.googleapis.com/container/instance_count%22&interval.startTime=${ONE_MINUTE_AGO}&interval.endTime=${NOW}" \
-  | jq -r '.timeSeries[]' | select(.labels.state == "active") | jq -r '.points[0].value.int64Value')
+  | jq -r '.timeSeries[] | select(.labels.state == "active")' | jq -r '.points[0].value.int64Value')
 
 echo "Pods: $PODS"
 
